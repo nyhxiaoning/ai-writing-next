32b10010ef1cccf6e21c78e43359a779cd6f1c8a
ningyongheng <ningyongheng@jeejio.com>
2024-09-11 14:47:21 +0800

    feat: 提交布局优化实现

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -1,13 +1,12 @@
 import FeaturesBg from "@/public/images/features-bg.png";
 import FeaturesBg1 from "@/public/images/features-bg春.jpeg";
-import FeaturesElement from "@/public/images/features-element.png";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function FeaturesBlocks() {
   return (
     <section className="relative">
-      {/* Section background (needs.relative class on parent and next sibling elements) */}
+      {/* Section background */}
       <div
         className="absolute inset-0 top-1/2 md:mt-24 lg:mt-0 bg-gray-900 pointer-events-none"
         aria-hidden="true"
@@ -23,275 +22,21 @@ export default function FeaturesBlocks() {
           </div>
 
           {/* Items */}
-          <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-7 lg:grid-cols-7 items-start md:max-w-2xl lg:max-w-none">
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-6">
             {/* 1st row */}
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            {/* 2nd row */}
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            {/* 3rd row */}
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            {/* 4th row */}
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
-            <Link href="/article-page">
-              <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer">
-                <Image
-                  className="md:max-w-none mx-auto rounded cursor-pointer"
-                  src={FeaturesBg1}
-                  width={100}
-                  height="162"
-                  alt="Features bg"
-                />
-              </div>
-            </Link>
+            {Array.from({ length: 24 }, (_, i) => (
+              <Link href="/article-page" key={i}>
+                <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl cursor-pointer ">
+                  <Image
+                    className="md:max-w-none mx-auto rounded cursor-pointer hover:scale-125"
+                    src={i % 2 === 0 ? FeaturesBg1 : FeaturesBg1}
+                    width={130}
+                    height="162"
+                    alt="Features bg"
+                  />
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
